a02c17dabf2668972ee961934522dcc888eb1051
Dipayan Biswas <dipayan1109033@gmail.com>
2024-10-28 20:10:32 -0500

    first commit

--- src/utils/converter.py
+++ src/utils/converter.py
@@ -76,6 +76,50 @@ def coco2bb(path, bb_type=BBType.GROUND_TRUTH):
             ret.append(bb)
     return ret
 
+def coco2bb_withoutPath(json_object, bb_type=BBType.GROUND_TRUTH):
+    ret = []
+    # COCO json file contains basically 3 lists:
+    # categories: containing the classes
+    # images: containing information of the images (width, height and filename)
+    # annotations: containing information of the bounding boxes (x1, y1, bb_width, bb_height)
+    classes = {}
+    if 'categories' in json_object:
+        classes = json_object['categories']
+        # into dictionary
+        classes = {c['id']: c['name'] for c in classes}
+    images = {}
+    # into dictionary
+    for i in json_object['images']:
+        images[i['id']] = {
+            'file_name': i['file_name'],
+            'img_size': (int(i['width']), int(i['height']))
+        }
+    annotations = []
+    if 'annotations' in json_object:
+        annotations = json_object['annotations']
+
+    for annotation in annotations:
+        img_id = annotation['image_id']
+        x1, y1, bb_width, bb_height = annotation['bbox']
+        if bb_type == BBType.DETECTED and 'score' not in annotation.keys():
+            print('Warning: Confidence not found in the JSON file!')
+            return ret
+        confidence = annotation['score'] if bb_type == BBType.DETECTED else None
+        # Make image name only the filename, without extension
+        img_name = images[img_id]['file_name']
+        img_name = general_utils.get_file_name_only(img_name)
+        # create BoundingBox object
+        bb = BoundingBox(image_name=img_name,
+                            class_id=classes[annotation['category_id']],
+                            coordinates=(x1, y1, bb_width, bb_height),
+                            type_coordinates=CoordinatesType.ABSOLUTE,
+                            img_size=images[img_id]['img_size'],
+                            confidence=confidence,
+                            bb_type=bb_type,
+                            format=BBFormat.XYWH)
+        ret.append(bb)
+    return ret
+
 
 def cvat2bb(path):
     '''This format supports ground-truth only'''
